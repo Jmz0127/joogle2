5ca0418d660ec6d9c0c6349f623fef09e3171e5b
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-16 18:39:36 -0500

    added header, search bar and enter button

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p className='Logo-header'>JOOGLE</p>
+				<div className='Search-field'>
+					<input placeholder='Type "jon_resume" here' />
+					<button>enter</button>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
